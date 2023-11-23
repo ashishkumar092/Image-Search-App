fa986b8755b87d5f65c86720c6c7e2edadd25a13
ashishkumar092 <yashishkumar8171@gmail.com>
2023-11-23 11:57:04 +0530

    add project

---
+++ src/components/searchBar.js
@@ -0,0 +1,17 @@
+// import React from 'react'
+// import { useRef } from 'react'
+// import styled from 'styled-components'
+
+// function searchBar(){
+//     const refe = useRef();
+//     function handleClick(){
+        
+//     }
+//     return <>
+//                 <input ref = {refe} type = "text" placeholder='Search Images'/>
+//                 <button onClick = {handleClick}>Search</button>
+
+//             </>
+// }
+
+// export default searchBar;
